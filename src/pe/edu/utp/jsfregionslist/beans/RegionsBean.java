e0514079261d4a2636b532ec477ea86bd501778a
iDarkcy <35429143+iDarkcy@users.noreply.github.com>
2018-02-17 21:37:12 -0500

    CRUD JSF

--- src/pe/edu/utp/jsfregionslist/beans/RegionsBean.java
+++ src/pe/edu/utp/jsfregionslist/beans/RegionsBean.java
@@ -12,11 +12,53 @@ import java.util.List;
 @SessionScoped
 public class RegionsBean implements Serializable {
     private HrService service;
+    private Region region;
 
     public  RegionsBean(){
         service=new HrService();
     }
     public List<Region> getRegions(){
         return service.findAllRegions();
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    public String getName() {
+        return this.getRegion().getName();
+    }
+    //patron de diseño delegate
+    public void setName(String name){
+        this.getRegion().setName(name);
+    }
+
+    public String newRegion(){
+        this.setRegion(new Region());
+        return "success";
+    }
+
+    public  String editRegion(Region region){
+        this.setRegion(region);
+        return "success";
+    }
+
+    public String createRegion(){
+        service.createRegion(this.getName());
+        return "success";
+    }
+
+    public String updateRegion(){
+        service.updateRegion(this.getRegion());
+        return "success";
+    }
+
+    public String deleteRegion(Region region){
+        service.deleteRegion(region.getId());
+        return "success";
     }
 }
